4e39b136e88e544242a7da262c64088c3557be2d
Harry Smith <harry@hsmith.net>
2021-01-04 16:51:24 +0000

    added option to change output

--- bin/screenwind.js
+++ bin/screenwind.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
 import puppeteer from 'puppeteer';
+import chalk from 'chalk';
+import yargs from 'yargs';
+import {
+  hideBin
+} from 'yargs/helpers';
 
 const tailwindDefaults = {
   theme: {
@@ -15,7 +21,7 @@ const tailwindDefaults = {
   }
 }
 
-const url = 'https://www.google.co.uk';
+const url = 'http://localhost:1313/';
 const universalHeight = 1000;
 
 const getScreenOptions = (config, height) => {
@@ -39,15 +45,21 @@ const getScreenOptions = (config, height) => {
   return options;
 }
 
-const takeScreenshots = async (page, options) => {
+const logScreenshot = (name, width, height, path) => {
+  console.log(
+    `${chalk.green('✓')} ${chalk.white.bold(name)} breakpoint | ${width}px x ${height}px @ ${path}`
+  )
+}
+
+const takeScreenshots = async (page, options, out) => {
   for (let option of options) {
-    const path = `./sw/${option.name}.png`;
+    const path = `${out}${option.name}.png`;
     const {
       width,
       height
     } = option;
 
-    console.log(`✓ ${option.name} breakpoint | ${width}px x ${height}px | ${path}`);
+    logScreenshot(option.name, width, height, path);
 
     // Set viewport size
     await page.setViewport({
@@ -58,12 +70,27 @@ const takeScreenshots = async (page, options) => {
     // Take the screenshot
     await page.screenshot({
       path: path,
-      fullPage: true
     });
   }
 }
 
-const run = async () => {
+const run = async ({
+  url,
+  out
+}) => {
+  // Create directory if it doesn't exist
+  fs.access(out, fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.mkdir(out, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`Making directory: ${out}`)
+        }
+      })
+    }
+  })
+
   console.log('Taking screenshots...')
   // Navigate to the page
   let browser = await puppeteer.launch({
@@ -77,10 +104,28 @@ const run = async () => {
 
   const options = getScreenOptions(tailwindDefaults, universalHeight);
 
-  await takeScreenshots(page, options);
+  await takeScreenshots(page, options, out);
 
   await page.close()
   await browser.close()
 }
 
-run();
+yargs(hideBin(process.argv))
+  .command('screenwind <url>', '',
+    (yargs) => {
+      yargs
+        .positional('url', {
+          describe: 'url to take screenshots of',
+
+        })
+        .option('out', {
+          alias: 'o',
+          describe: 'path to output files',
+          default: './sw/'
+        })
+    },
+    (argv) => {
+      run(argv)
+    })
+  .demandOption(['url'])
+  .argv;
